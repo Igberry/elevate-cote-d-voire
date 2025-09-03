2d3571e33e2f18ef1a0500bec67396ed4af6909e
Igberry <davidigberichuks@gmail.com>
2025-09-03 14:03:31 +0100

    save

--- scripts/script2.js
+++
@@ -1,7 +0,0 @@
-// Mobile nav toggle
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
-});

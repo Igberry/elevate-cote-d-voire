30cef5693e6bca9a544d201ed7877b02ce94280c
Igberry <davidigberichuks@gmail.com>
2025-09-03 13:32:06 +0100

    Elevate

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Mobile nav toggle
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
